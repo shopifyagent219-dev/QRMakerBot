07dfda4e433579f99d6cd23c73be366c2a8d2c76
shopifyagent219-dev <shopifyagent219@gmail.com>
2026-07-12 17:39:05 +0100

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,17 @@
+from .qr_handlers import (
+    start_command,
+    help_command,
+    text_qr_command,
+    url_qr_command,
+    wifi_qr_command,
+    contact_qr_command,
+    read_qr_command,
+    handle_message,
+    handle_callback,
+    handle_wifi_input,
+    handle_contact_input,
+    WAITING_WIFI_SSID,
+    WAITING_WIFI_PASSWORD,
+    WAITING_CONTACT_NAME,
+    WAITING_CONTACT_PHONE
+)
